bef6adfc5d60dab230ffdde40f695089905f2217
sawal123 <sawalinto@gmail.com>
2025-11-10 13:13:54 +0700

    FPanel

--- app/Models/User.php
+++ app/Models/User.php
@@ -25,7 +25,7 @@ class User extends Authenticatable
     ];
     public function canAccessPanel(Panel $panel): bool
     {
-        return str_ends_with($this->email, '@medantainment.com') && $this->hasVerifiedEmail();
+        return true;
     }
 
     /**

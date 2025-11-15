5d5e0957e168600d1947ef772aa4573786557a7d
sawal123 <sawalinto@gmail.com>
2025-11-15 13:56:32 +0700

    Feat : update model photo

--- app/Models/Photo.php
+++ app/Models/Photo.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Models;
-
+use App\Models\Client;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
